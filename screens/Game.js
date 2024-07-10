cbeeb23c175a42f0726c5b5f96be2389c70fe52f
cathyfu1215 <phoenixfucong@gmail.com>
2024-07-10 14:26:33 -0700

    added logic to game screen

--- screens/Game.js
+++ screens/Game.js
@@ -7,8 +7,16 @@ import { useState } from 'react'
 import { TextInput } from 'react-native-web'
 
 function Game() {
+
+  const [gameState,setGameState] = useState('guessing');// 'guessing', 'guessedWrong','win','lose'
+
+  const [hint, setHint] = useState('');
+  const [secretNumber, setSecretNumber] = useState(Math.floor(Math.random() * 100) + 1);
+  const [userGuess, setUserGuess] = useState('');
+
   const [attemptsLeft, setAttemptsLeft] = useState(4);
   const [timeLeft, setTimeLeft] = useState(60);
+
 
   function handleRestart(){
     console.log('handle restart')
@@ -22,12 +30,9 @@ function Game() {
     console.log('handle submit guess')
   }
 
-  return (
-    <View>
-        <View style={{alignItems:'flex-end'}}>
-          <MyButton title="Restart" onPress={handleRestart}/>
-        </View>
-        <Card>
+  function guessingCard(){
+    return (
+      <Card>
           <Text>Guess A Number Between 1 & 100</Text>
           <TextInput placeholder="" />
           <Text>Attempts left: {attemptsLeft}</Text>
@@ -37,6 +42,74 @@ function Game() {
             <MyButton title="Submit guess" onPress={handleSubmitGuess}/>
           </View>
         </Card>
+    )
+  }
+
+  function guessedWrongCard(){
+    return (
+      <Card>
+          <Text>Guess Wrong</Text>
+          <TextInput placeholder="" />
+          <Text>Attempts left: {attemptsLeft}</Text>
+          <Text>Timer: {timeLeft}</Text>
+          <View style={{ flexDirection: 'column', alignItems: 'center' }}>
+            <MyButton title="Use a Hint" onPress={handleHint}/>
+            <MyButton title="Submit guess" onPress={handleSubmitGuess}/>
+          </View>
+        </Card>
+    )
+  }
+
+  function winCard(){
+    return (
+      <Card>
+          <Text>You Win!</Text>
+          <Text>Attempts left: {attemptsLeft}</Text>
+          <Text>Timer: {timeLeft}</Text>
+          <View style={{ flexDirection: 'column', alignItems: 'center' }}>
+            <MyButton title="Use a Hint" onPress={handleHint}/>
+            <MyButton title="Submit guess" onPress={handleSubmitGuess}/>
+          </View>
+        </Card>
+    )
+  }
+
+  function loseCard(){
+    return (
+      <Card>
+          <Text>You Lose!</Text>
+          <Text>Attempts left: {attemptsLeft}</Text>
+          <Text>Timer: {timeLeft}</Text>
+          <View style={{ flexDirection: 'column', alignItems: 'center' }}>
+            <MyButton title="Use a Hint" onPress={handleHint}/>
+            <MyButton title="Submit guess" onPress={handleSubmitGuess}/>
+          </View>
+        </Card>
+    )
+  }
+
+
+  function rightCard(){
+    if(gameState === 'guessing'){
+      return guessingCard();
+    }
+    if(gameState === 'guessedWrong'){
+      return guessedWrongCard();
+    }
+    if(gameState === 'win'){
+      return winCard();
+    }
+    if(gameState === 'lose'){
+      return loseCard();
+    }
+  }
+  
+  return (
+    <View>
+        <View style={{alignItems:'flex-end'}}>
+          <MyButton title="Restart" onPress={handleRestart}/>
+        </View>
+        {rightCard()}
         
     </View>
      
